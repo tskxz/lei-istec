1e3f1053af86f22ad4e23cb26d0a99a64eadc4c8
Tanjil <tanjilkh@gmail.com>
2026-03-11 21:57:37 +0000

    updates

---
+++ P-II/ModeloConsola/ModeloConsola/Models/Pessoa.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModeloConsola.Models
+{
+    public class Pessoa
+    {
+        private string _nome;
+        private string _nif;
+        private int _anoNascimento;
+
+        public void Ler()
+        {
+            Console.Write("Nome: ");
+            _nome = Console.ReadLine();
+            Console.WriteLine("Nif: ");
+            _nif = Console.ReadLine();
+            Console.WriteLine("Ano de nascimento: ");
+            _anoNascimento = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public void Escrever()
+        {
+            int idade = 0;
+            idade = DateTime.Now.Year - _anoNascimento;
+            string saida = $"Nome: {_nome}\nNif: {_nif}\nAno de nascimento: {_anoNascimento}\nIdade: {idade}";
+            Console.WriteLine(saida);
+        }
+    }
+
+}
---
+++ P-II/ModeloConsola/ModeloConsola/Program.cs
@@ -0,0 +1,17 @@
+﻿using ModeloConsola.Models;
+
+namespace ModeloConsola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p;
+            p = new Pessoa();
+            // p._nome = "João";
+            p.Ler();
+            p.Escrever();
+            Console.ReadLine();
+        }
+    }
+}
