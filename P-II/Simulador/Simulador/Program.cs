42a92e546eef3755ac83538365a286f72d527778
Tanjil <tanjilkh@gmail.com>
2026-04-10 13:18:02 +0100

    push

---
+++ P-II/Simulador/Simulador/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Cryptography;
+using Simulador.Models;
+
+
+namespace Simulador
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Models.Simulador simulador = new Models.Simulador();
+            simulador.setAnoNascimento(2005);
+            decimal mensalidade = simulador.CalcularMensalidade();
+            simulador.ExibirDados();
+        }
+    }
+}
