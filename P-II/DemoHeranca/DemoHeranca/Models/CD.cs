d8e4c14d717d5b0b1b03ef83c7c732ea0b622b3a
Tanjil <tanjilkh@gmail.com>
2026-04-19 12:27:06 +0100

    push

---
+++ P-II/DemoHeranca/DemoHeranca/Models/CD.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoHeranca.Models
+{
+    public class CD : Conteudo
+    {
+        // nMusicas = 1
+        private int _nMusicas;
+        private double _duracao;
+
+        public void setNMusicas(int n) {
+            _nMusicas = n;
+        }
+
+        public void setDurancao(double duracao)
+        {
+            _duracao = duracao;
+        }
+
+        public CD() : base() {
+            setNMusicas(1);
+        }
+
+        public int getNMusicas() {
+            return _nMusicas;
+        }
+
+        public double getDuracao() {
+            return _duracao;
+        }
+        public override string show()
+        {
+            return $"{base.show()}\nN Musicas: {getNMusicas()}\nDuracao: {getDuracao()}";
+        }
+
+        public override string ToString()
+        {
+            return show();
+        }
+    }
+}
---
+++ P-II/DemoHeranca/DemoHeranca/Program.cs
@@ -0,0 +1,36 @@
+﻿using DemoHeranca.Models;
+namespace DemoHeranca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Conteudo conteudo = new Conteudo();
+             Console.WriteLine(conteudo.show());
+
+            Livro l = new Livro();
+            l.setTitulo("teste");
+            l.setAutor("xico");
+            Console.WriteLine(l.show());
+
+            Console.WriteLine(l.ToString());
+
+            Jornal j = new Jornal();
+            Console.WriteLine(j.show());
+
+            CD c = new CD();
+            Console.WriteLine(c.show());
+
+            List<Conteudo> festa = new List<Conteudo>();
+            festa.Add(l);
+            festa.Add(c);
+            festa.Add(j);
+
+            foreach (Conteudo objeto in festa){
+                Console.WriteLine(objeto.show());
+            }
+
+          
+        }
+    }
+}
