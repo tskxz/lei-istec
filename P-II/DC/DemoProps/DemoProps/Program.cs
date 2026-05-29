5619f51c0e55e3dcd3f2f053aded906cba91254c
Tanjil <tanjilkh@gmail.com>
2026-05-29 12:59:58 +0100

    push

---
+++ P-II/DC/DemoProps/DemoProps/Program.cs
@@ -0,0 +1,24 @@
+﻿using DemoProps.Models;
+
+namespace DemoProps
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto produto = new Produto();
+            // produto.setDesignacao("Coca-cola");
+            produto.Designacao = "Coca-Cola";
+            produto.IVA = 23.0M;
+            produto.setPrecoUnitario(1.5m);
+            produto.setQtdStock(100);
+            Console.WriteLine(produto.ToString());
+
+            Produto produto2 = new Produto();
+            produto2.Designacao = "Pepsi";
+            produto2.IVA = 1.00M;
+            Console.WriteLine(produto2.ToString());
+            Console.ReadKey();
+        }
+    }
+}
